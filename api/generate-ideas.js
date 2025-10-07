13ce4050c319b29232b94b3d6b59efc789a50165
Ali osman Arabacı <aliosmanarabaci@Ali-MacBook-Air.local>
2025-10-07 21:22:04 +0300

    fix: Convert API functions to CommonJS for Vercel compatibility

--- api/generate-ideas.js
+++ api/generate-ideas.js
@@ -1,6 +1,6 @@
-import { GoogleGenerativeAI } from '@google/genai';
+const { GoogleGenerativeAI } = require('@google/genai');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
